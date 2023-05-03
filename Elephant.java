d0a681db80ac79962be6cd7df02d6fcd0c918367
W-Dylan <131168232+W-Dylan@users.noreply.github.com>
2023-05-03 09:01:56 -0400

    Instantiate Elephant

--- Elephant.java
+++ Elephant.java
@@ -6,14 +6,15 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Elephant extends Actor
-{
+public class Elephant extends Actor{
     /**
      * Act - do whatever the Elephant wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    public void act()
-    {
-        // Add your action code here.
+    public void act(){
+    }
+    
+    // Constructor
+    public Elephant(){
     }
 }
