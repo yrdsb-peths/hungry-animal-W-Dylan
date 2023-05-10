a03e59ca563b59ed2eff28ca4476dda0d8cf14d7
W-Dylan <131168232+W-Dylan@users.noreply.github.com>
2023-05-10 08:40:25 -0400

    Moving right

--- Elephant.java
+++ Elephant.java
@@ -11,10 +11,18 @@ public class Elephant extends Actor{
      * Act - do whatever the Elephant wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    public void act(){
+    public void act(){ 
+        if(Greenfoot.isKeyDown("right")){
+                moveRight();
+            }
     }
     
     // Constructor
     public Elephant(){
+    }
+    
+    public void moveRight(){
+        setRotation(0);
+        move(10);
     }
 }
