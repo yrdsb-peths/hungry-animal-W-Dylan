9164f89e8bcdb12739718e1668d09a9c4deec2c2
W-Dylan <131168232+W-Dylan@users.noreply.github.com>
2023-05-29 09:03:44 -0400

    Basic Title Screen applied

---
+++ TitleScreen.java
@@ -0,0 +1,29 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class TitleScreen here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class TitleScreen extends World
+{
+    Label titleLabel = new Label("Hungry Elephant", 50);
+    /**
+     * Constructor for objects of class TitleScreen.
+     * 
+     */
+    public TitleScreen()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+        
+        addObject(titleLabel, 300, 200);
+    }
+    
+    public void act(){
+        if(Greenfoot.isKeyDown("enter")){
+            Greenfoot.setWorld(new MyWorld());
+        }
+    }
+}
